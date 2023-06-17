b207c5fd91a6aea5f200225deec89eab69586dca
Jigz <jigzpalillo@gmail.com>
2023-06-17 14:44:43 +0800

    [zero-app] Update build.rs

--- packages/zero-app/bin/run.rs
+++ packages/zero-app/bin/run.rs
@@ -1,3 +1,20 @@
+use std::process::Command;
+
+macro_rules! p {
+    ($($tokens: tt)*) => {
+        println!("cargo:warning={}", format!($($tokens)*))
+    }
+}
+
 fn main() {
-    println!("run!");
+    let tauri_dev = Command::new("cmd")
+        .arg("-c")
+        .arg("cargo tauri dev")
+        .output()
+        .unwrap();
+
+    let stdout = tauri_dev.stdout;
+    let s = String::from_utf8_lossy(&stdout);
+
+    p!("run! {}", s);
 }
