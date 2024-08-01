b3ab84cb61103c0bc1980c8fad566c957db39059
Jigz <jigzpalillo@gmail.com>
2024-08-01 20:49:44 +0800

    Fix height

--- apps/zero-app/src/App.tsx
+++ apps/zero-app/src/App.tsx
@@ -4,8 +4,8 @@ import { ThemeProvider } from '@/components/theme-provider'
 function App() {
   return (
     <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-      <div className='container flex'>
-        <img src='/zero-logo-v1.svg' alt='O' />
+      <div className='flex h-screen w-screen flex-col items-center justify-center'>
+        <img src='/zero-logo-v1.svg' alt='O' className='max-w-52' />
       </div>
     </ThemeProvider>
   )
