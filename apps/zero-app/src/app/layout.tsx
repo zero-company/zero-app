7c247ec5fcf8b2673b76010b901ed3272a332b98
Jigz <jigzpalillo@gmail.com>
2024-08-06 15:37:27 +0800

    Add window theme

--- apps/zero-app/src/app/layout.tsx
+++ apps/zero-app/src/app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
     <html lang='en' suppressHydrationWarning>
       <body
         className={cn(
-          'min-h-screen bg-background font-sans antialiased',
           //inter.className,
           fontSans.variable,
         )}
