80658c0f63a630db9574e199b7b8421f0911ab94
Jigz <jigzpalillo@gmail.com>
2024-08-26 23:42:45 +0800

    Update layout

---
+++ apps/zero-app/src/app/zero/layout.tsx
@@ -0,0 +1,55 @@
+'use client'
+import { AppLayout, Icon, ZeroLogo } from '@zero-company/zero-lib-react'
+import {
+  LuSettings,
+  LuUserCircle2,
+  LuSearch,
+  LuList,
+  LuHome,
+} from 'react-icons/lu'
+
+type Props = {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <AppLayout
+        body={children}
+        header={
+          <div className='flex-1 p-2'>
+            <p>zero</p>
+          </div>
+        }
+        sidebarTabs={{
+          defaultTab: 'home',
+          top: [
+            {
+              id: 'home',
+              icon: <Icon reactIcon={<LuHome />} />,
+              content: <div>home</div>,
+            },
+          ],
+          bottom: [
+            {
+              id: 'search',
+              icon: <Icon reactIcon={<LuSearch />} />,
+              content: <div>search</div>,
+            },
+            {
+              id: 'user',
+              icon: <Icon reactIcon={<LuUserCircle2 />} />,
+              content: <div>user</div>,
+            },
+            {
+              id: 'settings',
+              icon: <Icon reactIcon={<LuSettings />} />,
+              content: <div>settings</div>,
+            },
+          ],
+        }}
+      />
+    </>
+  )
+}
