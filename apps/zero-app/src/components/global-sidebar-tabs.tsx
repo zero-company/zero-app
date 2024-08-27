13f22445dea5c6e67f965dbfd45b084e8df66cde
Jigz <jigzpalillo@gmail.com>
2024-08-27 20:59:40 +0800

    Update GlobalSidebarTabs

--- apps/zero-app/src/components/global-sidebar-tabs.tsx
+++ apps/zero-app/src/components/global-sidebar-tabs.tsx
@@ -25,15 +25,18 @@ export const GlobalSidebarTabs = [
     id: 'search',
     icon: <Icon reactIcon={<LuSearch />} />,
     content: <div>search</div>,
+    disabled: true,
   },
   {
     id: 'user',
     icon: <Icon reactIcon={<LuUserCircle2 />} />,
     content: <div>user</div>,
+    disabled: true,
   },
   {
     id: 'settings',
     icon: <Icon reactIcon={<LuSettings />} />,
     content: <div>settings</div>,
+    disabled: true,
   },
 ]
