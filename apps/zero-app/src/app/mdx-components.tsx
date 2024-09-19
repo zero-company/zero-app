9aeab1a2877497fc8cc07664a94432431a4a2155
Jigz <jigzpalillo@gmail.com>
2024-09-19 23:00:27 +0800

    Use slugify

--- apps/zero-app/src/app/mdx-components.tsx
+++ apps/zero-app/src/app/mdx-components.tsx
@@ -1,24 +1,15 @@
 import type { MDXComponents } from 'mdx/types'
 import Link from 'next/link'
+import slugify from '@sindresorhus/slugify'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h2: ({ children }) => (
       <Link
-        href='#'
+        href={`#${slugify(children?.toString() || '')}`}
         className='underline underline-offset-4 hover:text-primary'
       >
-        <h2
-          id={children
-            ?.toString()
-            .toLowerCase()
-            .trim()
-            .replace(/[^\w\s-]/g, '')
-            .replace(/[\s_-]+/g, '-')
-            .replace(/^-+|-+$/g, '')}
-        >
-          {children}
-        </h2>
+        <h2 id={slugify(children?.toString() || '')}>{children}</h2>
       </Link>
     ),
     ...components,
