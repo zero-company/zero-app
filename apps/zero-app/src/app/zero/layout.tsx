a198db827931ecb10ae33f0ddf96b7123435427f
Jigz <jigzpalillo@gmail.com>
2024-09-26 00:52:45 +0800

    Add SocialLinks

--- apps/zero-app/src/app/zero/layout.tsx
+++ apps/zero-app/src/app/zero/layout.tsx
@@ -9,6 +9,8 @@ import {
   Button,
   Menu,
   ZeroLogo,
+  ZERO_LINKS,
+  GradientButton,
 } from '@zero-company/zero-lib-react'
 import {
   LuList,
@@ -20,11 +22,41 @@ import {
   LuBookOpen,
   LuInfo,
 } from 'react-icons/lu'
+import { FaFacebookF, FaGithub, FaTwitter, FaGlobe } from 'react-icons/fa6'
 import { GlobalSidebarTabs } from '@/components'
 
 type Props = {
   children: React.ReactNode
 }
+
+const SocialLinks = () => (
+  <>
+    <div className='flex p-2 gap-2'>
+      <Link href={ZERO_LINKS.buymeacoffee} className='flex-1' target='_blank'>
+        <GradientButton className='w-full'>Support Zero</GradientButton>
+      </Link>
+      <Link href={ZERO_LINKS.discord} className='flex-1' target='_blank'>
+        <GradientButton gradient='purple2' className='w-full'>
+          Join Community
+        </GradientButton>
+      </Link>
+    </div>
+    <div className='h-8 divide-x flex *:flex *:flex-1 *:justify-center *:h-full *:p-2'>
+      <Link href={ZERO_LINKS.website} className='flex-1' target='_blank'>
+        <IconV2 size='sm' reactIcon={<FaGlobe />} />
+      </Link>
+      <Link href={ZERO_LINKS.github} className='flex-1' target='_blank'>
+        <IconV2 size='sm' reactIcon={<FaGithub />} />
+      </Link>
+      <Link href={ZERO_LINKS.twitter} className='flex-1' target='_blank'>
+        <IconV2 size='sm' reactIcon={<FaTwitter />} />
+      </Link>
+      <Link href={ZERO_LINKS.facebook} className='flex-1' target='_blank'>
+        <IconV2 size='sm' reactIcon={<FaFacebookF />} />
+      </Link>
+    </div>
+  </>
+)
 
 export default function Layout({ children }: Props) {
   return (
@@ -90,7 +122,7 @@ export default function Layout({ children }: Props) {
         }
         footer={
           <>
-            <SupportCard />
+            <SocialLinks />
             <Footer />
           </>
         }
